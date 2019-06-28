06903cae34b951b1f002be307ad79ff252f2d2d0
maximo <el.cano00@hotmail.es>
2019-06-27 23:29:38 -0400

    version con listview
    esta es la version con el list view puesto

--- app/src/main/java/com/example/login3/segunda.java
+++ app/src/main/java/com/example/login3/segunda.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class segunda extends AppCompatActivity {
 
@@ -14,6 +17,17 @@ public class segunda extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        String [ ] personajes = { "Juan Pablo ", "Pedro Bone", "Jacinto Peinado ", "Joangel Reyes"};
+
+        ListAdapter buckyysAdap = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, personajes);
+        ListView listView1 = (ListView) findViewById(R.id.listView1);
+
+        listView1.setAdapter(buckyysAdap);
+
+
+
+
 
         ir3 = (ImageView) findViewById(R.id.imageView);
 
